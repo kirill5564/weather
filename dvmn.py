35a5e9def221b206c886e860518ee1dfc26dda62
kirill5564 <Krshemanaev1@mail.ru>
2025-03-28 00:49:07 +0300

    Update dvmn.py

--- dvmn.py
+++ dvmn.py
@@ -2,15 +2,11 @@ import requests
 
 
 if __name__ == '__main__':
-    url_london = 'https://wttr.in/london?nTqu&lang=en'
-    response = requests.get(url_london)
-    response.raise_for_status()
-    print(response.text)
-    url_sheremetyevo = 'https://wttr.in/svo?nTqu&lang=en'
-    response = requests.get(url_sheremetyevo)
-    response.raise_for_status()
-    print(response.text)
-    url_сherepovets = 'https://wttr.in/Череповец?nTqM&lang=ru'
-    response = requests.get(url_сherepovets)
-    response.raise_for_status()
-    print(response.text)
+    urls = ['https://wttr.in/london',
+           'https://wttr.in/svo',
+           'https://wttr.in/Череповец']
+    for url in urls:
+        payload = {"nTqM": "", "lang": "ru"}
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        print(response.text)
